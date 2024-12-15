25d5791fe4e219fbf0c01324762a4fff507da6e4
rjmahfuztech <rjmahfuztech@gmail.com>
2024-12-15 18:47:56 +0600

    Subject class added

---
+++ subject.py
@@ -0,0 +1,15 @@
+from person import Teacher
+from school import School
+class Subject:
+    def __init__(self,name,teacher):
+        self.name = name
+        self.teacher = teacher # teacher ar object
+        self.max_marks = 100
+        self.pass_marks = 33
+
+    def exam(self,students): # student ar list
+        for student in students:
+            mark = Teacher.evaluate_exam()
+            student.marks[self.name] = mark
+            student.subject_grade[self.name] = School.calculate_grade(mark)
+
